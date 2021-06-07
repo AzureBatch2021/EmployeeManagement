6b72968d29975c4b764a839727b1cc2b3ff9d13f
sandhya <sandhyasivalasetti06@gmail.com>
2021-06-07 17:07:16 +0530

    updated by sandhya

--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -14,7 +14,7 @@ namespace WindowsFormsApp1
         [STAThread]
         static void Main()
         {
-            
+            //This is for testing
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
